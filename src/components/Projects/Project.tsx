97b72c98fda245d4178ab0a79990c795d03b7be5
Rudra Raj Shrivastava <rudrii1807@gmail.com>
2024-11-12 15:48:17 +0530

    Changes Done

--- src/components/Projects/Project.tsx
+++ src/components/Projects/Project.tsx
@@ -32,7 +32,7 @@ interface ProjectProps {
     className={`${
       show ? "opacity-80 scale-100" : "opacity-0 scale-95"
     }pointer-events-none transition-opacity duration-300 absolute bg-black top-0 w-full h-full rounded-lg flex items-center justify-center`}
-  >      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className={`${show?"w-20 h-20":"w-16 h-16"} transition-all duration-300 tabler-icon tabler-icon-external-link m-auto size-10 stroke-white scale-50 group-hover:scale-100`} viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-external-link m-auto size-10 stroke-white transition-all scale-50 group-hover:scale-100"><path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6"></path><path d="M11 13l9 -9"></path><path d="M15 4h5v5"></path></svg>
+  >      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className={`${show?"w-20 h-20":"w-16 h-16"} transition-all duration-300 tabler-icon tabler-icon-external-link m-auto size-10 stroke-white scale-50 group-hover:scale-100`} viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6"></path><path d="M11 13l9 -9"></path><path d="M15 4h5v5"></path></svg>
         </a> 
         </div>
   
